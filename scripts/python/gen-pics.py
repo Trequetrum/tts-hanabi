5eb972a34eb0084caaa4f3919ad03e9701b8556c
Trequetrum <trequetrum@gmail.com>
2022-03-08 21:14:26 -0500

    Create gen-pics.py

---
+++ scripts/python/gen-pics.py
@@ -0,0 +1,63 @@
+from math import floor
+from PIL import Image
+from itertools import chain, combinations
+
+prefix_img_str = "../../assets/"
+
+# Load Blank Backing
+back_blank_img = Image.open(f"{prefix_img_str}back_blank.png").convert("RGBA")
+back_blank_width, back_blank_height = back_blank_img.size
+
+# Load numbers to overlay backing
+back_num_imgs = list(map(
+    lambda n: {
+        "name": f"{n+1}",
+        "img": Image.open(f"{prefix_img_str}back_{n+1}.png").convert("RGBA")
+    },
+    range(5)
+))
+
+# Position numbers to overlay backing
+for img_ref in back_num_imgs:
+    width, height = img_ref['img'].size
+    img_ref['pos'] = (
+        floor(back_blank_width / 2) - floor(width / 2),
+        back_blank_height - 200 + floor(height/2)
+    )
+
+# Load color swatches to overlay backing
+back_colr_imgs = list(map(
+    lambda c: {
+        "name": c,
+        "img": Image.open(f"{prefix_img_str}back_{c}.png").convert("RGBA")
+    }, 
+    ['b','g','r','w','y']
+))
+
+# Position color swatches to overlay backing
+for i, img_ref in enumerate(back_colr_imgs):
+    width, height = img_ref['img'].size
+    if(i < 3):
+        img_ref['pos'] = (
+            (floor(back_blank_width / 4) * (i + 1)) - floor(width / 2),
+            170 - floor(height/2)
+        )
+    else:
+        img_ref['pos'] = (
+            (floor(back_blank_width / 3) * (i - 2)) - floor(width / 2),
+            70 - floor(height/2)
+        )
+
+def all_subsets(ss):
+    return chain(*map(lambda x: combinations(ss, x), range(0, len(ss)+1)))
+
+for color_subset in all_subsets(back_colr_imgs):
+    for num_img in back_num_imgs:
+        back_blank = back_blank_img.copy()
+        back_blank.paste(num_img["img"], num_img["pos"], mask = num_img["img"])
+        color_string = ""
+        for color_img in color_subset:
+            color_string += color_img['name']
+            back_blank.paste(color_img["img"], color_img["pos"], mask = color_img["img"])
+
+        back_blank.save(f"{prefix_img_str}generated/{num_img['name']}{color_string}.png")
