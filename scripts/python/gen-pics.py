7c2af89c92030fd3e36f10914f16fa55bb387bfe
Trequetrum <trequetrum@gmail.com>
2022-03-31 14:15:20 -0400

    Images v3 for less visual clutter on the tabletop

--- scripts/python/gen-pics.py
+++ scripts/python/gen-pics.py
@@ -2,16 +2,24 @@ from math import floor
 from PIL import Image
 from itertools import chain, combinations
 
-def thumbnail_image(img_str, size):
+def resize_image(img_str, size):
     img = Image.open(img_str)
-    img.thumbnail(size, reducing_gap=3.0)
-    return img.convert("RGBA")
+    max_width, max_height = size
+    width, height = img.size
+    new_ratio = min(max_width/width, max_height/height)
+    new_size = floor(width * new_ratio), floor(height * new_ratio)
+
+    return img.resize(
+        new_size, 
+        resample=Image.BICUBIC, 
+        reducing_gap=3.0
+    ).convert("RGBA")
 
 
 prefix_img_str = "../../assets/"
 
 # Load Blank Backing
-back_blank_img = Image.open(f"{prefix_img_str}back_blank_v2.png").convert("RGBA")
+back_blank_img = Image.open(f"{prefix_img_str}back_blank_grey_v3.png").convert("RGBA")
 back_blank_width, back_blank_height = back_blank_img.size
 
 thumbnail_size = floor(back_blank_width/3), floor(back_blank_height/3)
@@ -20,7 +28,7 @@ thumbnail_size = floor(back_blank_width/3), floor(back_blank_height/3)
 back_num_imgs = list(map(
     lambda n: {
         "name": f"{n+1}",
-        "img": thumbnail_image(f"{prefix_img_str}back_{n+1}_v2.png", thumbnail_size)
+        "img": resize_image(f"{prefix_img_str}back_{n+1}_v3.png", thumbnail_size)
             .convert("RGBA")
     },
     range(5)
@@ -28,32 +36,36 @@ back_num_imgs = list(map(
 
 # Position numbers to overlay backing
 for img_ref in back_num_imgs:
+    width,height = img_ref['img'].size
     img_ref['pos'] = (
-        floor(back_blank_width / 2) - floor(thumbnail_size[0] / 2),
-        back_blank_height - thumbnail_size[1]
+        floor(back_blank_width / 2 - width / 2),
+        back_blank_height - height
     )
 
 # Load color swatches to overlay backing
 back_colr_imgs = list(map(
     lambda c: {
         "name": c,
-        "img": thumbnail_image(f"{prefix_img_str}back_{c}_v2.png", thumbnail_size)
+        "img": resize_image(f"{prefix_img_str}back_{c}_v3.png", thumbnail_size)
     }, 
     ['b','g','r','w','y']
 ))
 
 # Position color swatches to overlay backing
 for i, img_ref in enumerate(back_colr_imgs):
+    width,height = img_ref['img'].size
+    x_diff = floor((thumbnail_size[0] - width) / 2)
+    y_diff = floor((thumbnail_size[1] - height) / 2)
     if(i < 3):
         img_ref['pos'] = (
-            thumbnail_size[0] * i,
-            0 # floor(thumbnail_size[1]/2)
+            (thumbnail_size[0] * i) + x_diff,
+            0 + y_diff
         )
     else:
         offset = i % 3
         img_ref['pos'] = (
-            (floor(back_blank_width / 3) * (i - 2)) - floor(thumbnail_size[0] / 2),
-            thumbnail_size[1] - floor(thumbnail_size[1]/3)
+            floor(((back_blank_width / 3) * (i - 2)) - (thumbnail_size[0] / 2)) + x_diff,
+            floor(thumbnail_size[1] + y_diff)
         )
 
 # Return an iterable over all combinations of the input iterable
@@ -71,9 +83,9 @@ for color_subset in all_subsets(back_colr_imgs):
         color_string += color_img['name']
         back_blank.paste(color_img["img"], color_img["pos"], mask = color_img["img"])
     
-    back_blank.save(f"{prefix_img_str}generated/back_{color_string}.png")
+    back_blank.save(f"{prefix_img_str}generated/back_{color_string}_v3.png")
 
     for num_img in back_num_imgs:
         back_blank_colored = back_blank.copy()
         back_blank_colored.paste(num_img["img"], num_img["pos"], mask = num_img["img"])
-        back_blank_colored.save(f"{prefix_img_str}generated/back_{num_img['name']}{color_string}.png")
+        back_blank_colored.save(f"{prefix_img_str}generated/back_{num_img['name']}{color_string}_v3.png")
